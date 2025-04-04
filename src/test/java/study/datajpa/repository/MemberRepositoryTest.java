0df2ab82c3d5fca1d3ab9b63b6dcfec2dbeff4ca
sspark <sauce0127@gmail.com>
2025-04-04 17:31:14 +0900

    feat : hint & lock

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -230,6 +230,31 @@ class MemberRepositoryTest {
         }
     }
 
+    @Test
+    void queryHint() {
+        // given
+        Member member = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    void testLock() {
+        // given
+        Member member = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> memberList = memberRepository.findLockByUsername("member1");
+    }
+
 
 
 

fe7a8f3530823723618d41000bd46e4bb3e3c1df
sspark <sauce0127@gmail.com>
2025-04-04 19:27:28 +0900

    feat : WEB 확장 기능
    - 도메인 클래스 컨버터
    - 페이징과 정렬

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,49 @@
+package study.datajpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    // http://localhost:8080/members?page=1&size=3&sort=id,desc
+    // pageable 을 사용한 페이지 객체 반환
+    // 페이징을 1 부터 하고싶다면 Pageable 재정의 또는 one-indexed-parameters: true
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(MemberDto::new);
+    }
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i<100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+}

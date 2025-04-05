f294153a7e8beea716d65d741586d07bfd77af74
sspark <sauce0127@gmail.com>
2025-04-05 12:19:09 +0900

    feat : isNew(), 새로운 엔티티를 구별하는 방법

---
+++ src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,43 @@
+package study.datajpa.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+
+    @Id
+//    @GeneratedValue
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+}
